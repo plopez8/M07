e154c6021a200f40b55ccc6a479aa76cfdcdb36e
plopez8 <102725933+plopez8@users.noreply.github.com>
2022-10-24 17:55:31 +0200

    Add files via upload

---
+++ examen/hola.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <title>Benvingut</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+
+</head>
+<body>
+<div class="container noheight" id="container">
+    <div class="welcome-container">
+        <h1>Benvingut!</h1>
+        <div>Hola Aniol, les teves darreres connexions són:</div>
+        <form action="#">
+            <button>Tanca la sessió</button>
+        </form>
+    </div>
+</div>
+</body>
+</html>
---
+++ examen/process.php
@@ -0,0 +1,77 @@
+<?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        echo('halo');
+        if($_POST['method'] == 'signup'){
+            echo('singup');
+            crearusuari();
+        }
+        if($_POST['method'] == 'signin'){
+            echo('singin');
+            comprobarusuari();
+        }
+        
+        // echo($_POST['method']);
+        // crearusuari();
+        // comprobarusuari();
+
+    }
+//crea l'usuari si no hi ha cap usuari igual;
+    function crearusuari(){
+        
+        $nom = $_POST['nom'];
+        $gmail = $_POST['email'];
+        $contrasenya = $_POST['pwd'];
+        $json = file_get_contents('users.json');
+        $json_data = json_decode($json,true);
+        if($json_data == null){
+            $myArr = array('0', '0', '0');
+            $json_data[] = $myArr;
+            $myJSON = json_encode($json_data);
+            file_put_contents('users.json', $myJSON);
+        }
+        $igual = 0;
+        if(is_null($contrasenya)){
+            header("Location: index.php");
+        }else{   
+        for ($i=0; $i< count($json_data); $i++){
+            if ($json_data[$i][1] == $gmail){
+                $igual++;
+            }
+        }
+    }
+        if ($igual == 0){
+            $myArr = array($nom, $gmail, $contrasenya);
+            $json_data[] = $myArr;
+            $myJSON = json_encode($json_data);
+            file_put_contents('users.json', $myJSON);
+            header("Location: hola.php");
+        }else{
+            header("Location: index.php");
+        }
+    }
+
+    function comprobarusuari(){
+        $gmail = $_POST['email'];
+        $contrasenya = $_POST['pwd'];
+        $json = file_get_contents('users.json');
+        $json_data = json_decode($json,true);
+        $igual = 0;
+        for ($i=0; $i< count($json_data); $i++){
+            if (($json_data[$i][1] == $gmail) && ($json_data[$i][2] == $contrasenya)){
+                $igual++;
+                $nom = $json_data[$i][0];
+            }
+        }
+        if ($igual == 1){
+            header("Location: hola.php");
+            
+        } else {
+            header("Location: index.php");
+        }
+    }
+
+    function mostrarnom($nom){
+        echo($_POST['nom']);
+        return "Hola".$_POST['nom'].", les teves darreres connexions són: ";
+    }
+?>
